3444caa0b3d3de66febd588d94a890d4238de76d
nijakow <79372954+nijakow@users.noreply.github.com>
2024-10-11 21:12:29 +0000

    Fixed the config for FreeBSD

--- init.el
+++ init.el
@@ -1,8 +1,10 @@
 
+(normal-erase-is-backspace-mode (if window-system 0 1)) ;; Ensure the backspace key works on FreeBSD
+
 (setq make-backup-files nil)
 
 (delete-selection-mode 1)
-(menu-bar-mode 1)
+(menu-bar-mode 0)
 (tool-bar-mode 0)
 
 (global-set-key [C-tab] 'other-window)
