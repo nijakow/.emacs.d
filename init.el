65d2d19ec73a27a10b15f31328fc0822068e2b56
nijakow <79372954+nijakow@users.noreply.github.com>
2024-10-14 21:58:20 +0200

    Extended init.el

---
+++ init.el
@@ -0,0 +1,10 @@
+
+(setq make-backup-files nil)
+
+(delete-selection-mode 1)
+(menu-bar-mode 1)
+(tool-bar-mode 0)
+
+(global-set-key [C-tab] 'other-window)
+(global-set-key [C-iso-lefttab] 'buffer-menu)
+(global-set-key [home] 'buffer-menu)
