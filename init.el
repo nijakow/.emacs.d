9044458229e2a95d452709075c261256575ce5de
nijakow <79372954+nijakow@users.noreply.github.com>
2025-01-18 17:55:21 +0100

    Minor fixes

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 
-(normal-erase-is-backspace-mode (if window-system 0 1)) ;; Ensure the backspace key works on FreeBSD
+;; (normal-erase-is-backspace-mode (if window-system 0 1)) ;; Ensure the backspace key works on FreeBSD
 
 (setq make-backup-files nil)
 
@@ -8,7 +8,7 @@
 (tool-bar-mode 0)
 
 (global-set-key [C-tab] 'other-window)
-(global-set-key [C-iso-lefttab] 'buffer-menu)
+(global-set-key [C-iso-lefttab] 'previous-window-any-frame)
 (global-set-key [home] 'buffer-menu)
 
 (setq c-default-style "linux"
