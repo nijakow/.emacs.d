90afeef0619194e359fd637f592113a17e59514b
nijakow <79372954+nijakow@users.noreply.github.com>
2025-01-18 16:02:41 +0100

    Selected a theme

--- init.el
+++ init.el
@@ -60,7 +60,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(smalltalk-mode)))
+ '(custom-enabled-themes '(doom-flatwhite))
+ '(custom-safe-themes
+   '("a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
